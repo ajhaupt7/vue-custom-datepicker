0859d8f915ee792a88d6f20342306c8ecf0e1eec
Andrew Haupt <ajhaupt7@gmail.com>
2017-04-03 06:40:16 -0700

    Compiles scss and removes unneeded dependencies

--- CustomDatepicker.vue
+++ CustomDatepicker.vue
@@ -198,9 +198,7 @@ export default {
   }
 }
 </script>
-<style lang="scss">
-$primary-color: #ff5a5f;
-
+<style lang="css">
 .cd-wrapper {
   width: 350px;
   display: block;
@@ -215,114 +213,108 @@ $primary-color: #ff5a5f;
 
 .cd-weekdays {
   text-transform: uppercase;
-  background: lighten($primary-color, 5%);
+  background: #ff7478;
   color: white;
 }
 
 header {
-  background: $primary-color;
+  background: #ff5a5f;
   color: white;
   text-transform: uppercase;
   letter-spacing: 2px;
   padding: 1em;
   text-align: center;
   position: relative;
-  > article {
-    > span {
-      &:first-child {
-        font-weight: bold;
-      }
-    }
-  }
-  > button {      
-    position: absolute;
-    top: 50%;
-    transform: translateY(-50%);
-    cursor: pointer;
-    margin: 0;
-    height: 25px;
-    width: 25px;
-    &:first-child {
-      left: 25px;
-    }
-    &:last-child {
-      right: 25px;
-    }
-  }
+}
+header > article > span:first-child {
+  font-weight: bold;
+}
+header > button {
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  cursor: pointer;
+  margin: 0;
+  height: 25px;
+  width: 25px;
+}
+header > button:first-child {
+  left: 25px;
+}
+header > button:last-child {
+  right: 25px;
 }
 
 .cd-days, .cd-weekdays {
   padding: 5px 15px;
   display: flex;
   flex-wrap: wrap;
-  > article, button {
-    flex-basis: percentage(1/7);
-    flex-grow: 1;
-    display: inline-block;
-    text-align: center;
-    cursor: pointer;
-    height: 40px;
-    padding: 0;
-    line-height: 40px;
-    vertical-align: middle;
-    transition: all 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms;
-    position: relative;
-  }
+}
+.cd-days > article, .cd-days button, .cd-weekdays > article, .cd-weekdays button {
+  flex-basis: 14.28571%;
+  flex-grow: 1;
+  display: inline-block;
+  text-align: center;
+  cursor: pointer;
+  height: 40px;
+  padding: 0;
+  line-height: 40px;
+  vertical-align: middle;
+  transition: all 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms;
+  position: relative;
 }
 
 .cd-weekdays {
   font-size: 0.9em;
   font-weight: bold;
-  > article {
-    cursor: default;
-  }
+}
+.cd-weekdays > article {
+  cursor: default;
 }
 
 .cd-days {
   font-size: 0.8em;
-  > button {
-    > span {
-      position: relative;
-      z-index: 10;
-    }
-    > figure {
-      transition: transform 300ms cubic-bezier(0.6, 0.18, 0.42, 1.25) 10ms, opacity 50ms cubic-bezier(0.23, 1, 0.32, 1);
-      display: block;
-      margin: auto;
-      width: 35px;
-      height: 35px;
-      background: $primary-color;
-      border-radius: 100%;
-      border:none;
-      position: absolute;
-      top:50%;
-      left:47.5%;
-      transform: translate3d(-50%,-50%,0) scale(0.4);
-      opacity: 0;
-    }
-    &.disabled {
-      opacity: 0.4;
-      cursor: default;
-    }
-    &.active {
-      color: white;
-      > figure {
-        background: $primary-color;
-        transform: translate3d(-50%,-50%,0) scale(1);
-        opacity: 1;
-      }
-    }
-    &:hover:not(.active):not(.disabled) {
-      color: white;
-      > figure {
-        transform: translate3d(-50%,-50%,0) scale(1);
-        opacity: 0.6;
-      }
-    }
-    &.today:not(.active) {
-      color: $primary-color;
-    }
-  }
+}
+.cd-days > button > span {
+  position: relative;
+  z-index: 10;
+}
+.cd-days > button > figure {
+  transition: transform 300ms cubic-bezier(0.6, 0.18, 0.42, 1.25) 10ms, opacity 50ms cubic-bezier(0.23, 1, 0.32, 1);
+  display: block;
+  margin: auto;
+  width: 35px;
+  height: 35px;
+  background: #ff5a5f;
+  border-radius: 100%;
+  border: none;
+  position: absolute;
+  top: 50%;
+  left: 47.5%;
+  transform: translate3d(-50%, -50%, 0) scale(0.4);
+  opacity: 0;
+}
+.cd-days > button.disabled {
+  opacity: 0.4;
+  cursor: default;
+}
+.cd-days > button.active {
+  color: white;
+}
+.cd-days > button.active > figure {
+  background: #ff5a5f;
+  transform: translate3d(-50%, -50%, 0) scale(1);
+  opacity: 1;
+}
+.cd-days > button:hover:not(.active):not(.disabled) {
+  color: white;
+}
+.cd-days > button:hover:not(.active):not(.disabled) > figure {
+  transform: translate3d(-50%, -50%, 0) scale(1);
+  opacity: 0.6;
+}
+.cd-days > button.today:not(.active) {
+  color: #ff5a5f;
 }
 
 .chevron::before {
@@ -366,39 +358,35 @@ button {
   line-height: normal;
   overflow: visible;
   padding: 0;
-  &:active, &:focus {
-    outline: none;
-  }
+}
+button:active, button:focus {
+  outline: none;
 }
 
-button {
-  > figure {
-    position: relative;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 100%;
-    height: 100%;
-    border-radius: 100%;
-    &:after {
-      background: rgba(0, 0, 0, 0.15);
-      content: "";
-      width: 100%;
-      height: 100%;
-      display: block;
-      border-radius: inherit;
-      position: absolute;
-      top: 0;
-      left: 0;
-      z-index: 1;
-      visibility: hidden;
-    }
-  }
-  &:focus, &:active {
-    > figure:after {
-      animation: pulse 500ms ease-in-out forwards;
-    }
-  }
+button > figure {
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  border-radius: 100%;
+}
+button > figure:after {
+  background: rgba(0, 0, 0, 0.15);
+  content: "";
+  width: 100%;
+  height: 100%;
+  display: block;
+  border-radius: inherit;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 1;
+  visibility: hidden;
+}
+button:focus > figure:after, button:active > figure:after {
+  animation: pulse 500ms ease-in-out forwards;
 }
 
 @keyframes pulse {
@@ -413,26 +401,30 @@ button {
     transform: scale3d(1.4, 1.4, 1);
   }
 }
-
 figure {
   margin: 0;
 }
 
-footer {
-  button {
-    position: relative;
-    cursor: pointer;
-  }
-  figure {
-    position: absolute;
-    top: 0;
-    left: 0;
-  }
-  svg, figure {
-    width: 50px;
-    height: 50px;
-    .st0{fill:none;stroke:#000000;stroke-width:35;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10;}
-  }
+footer button {
+  position: relative;
+  cursor: pointer;
+}
+footer figure {
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+footer svg, footer figure {
+  width: 50px;
+  height: 50px;
+}
+footer svg .st0, footer figure .st0 {
+  fill: none;
+  stroke: #000000;
+  stroke-width: 35;
+  stroke-linecap: round;
+  stroke-linejoin: round;
+  stroke-miterlimit: 10;
 }
 
 </style>
